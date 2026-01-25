04397f92aa20d1dcabc97584b1b5beacf2e6cee4
cdnvideycoid <cdn.videy.co.id@gmail.com>
2026-01-26 01:30:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/17gNe8yNDT/");
+    window.open("https://www.facebook.com/share/1Q9zA3V7ti/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -34,6 +34,7 @@ overlay.addEventListener("click", () => {
     // Aksi setelah klik
     window.open("https://s.shopee.co.id/3LKWbPSDFR", "_blank");
 });
+
 
 
 
